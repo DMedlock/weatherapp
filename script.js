3ed1e1317a7bcb6c8e44e7d139de960d81938d04
DMedlock <danielleymedlock@gmail.com>
2022-09-15 12:04:54 -0500

    built api for weekly forecast

--- script.js
+++ script.js
@@ -19,23 +19,38 @@ let day = days[now.getDay()];
 
 h3.innerHTML = `${day} ${hour}:${minutes}`;
 
-function displayForecast() {
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri" ,"Sat"];
+
+
+return days[day];
+
+
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row">`;
-  let days = ["Mon", "Tues", "Wed", "Thur", "Fri"];
-  days.forEach(function (day) {
+  forecast.forEach (function (forecastDay, index) {
+    if (index < 5) {
     forecastHTML =
       forecastHTML +
       `<div class="col">
       <div class="weekday">
-        ${day}
+        ${formatDay(forecastDay.dt)}
       </div>
-      <img src="images/overcast clouds.png" alt="sun" class="mini-icon" />
+      <img src="images/${forecastDay.weather[0].description}.png" class="mini-icon" />
       <div class="temp">
-        80°
+        ${
+          Math.round(forecastDay.temp.max)}°
       </div>
   </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -43,8 +58,9 @@ function displayForecast() {
 }
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "da26e26f0ab947650766187ca3c0290f";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let apiKey = "1dbf926d3b4417bf379db7043bec1047";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
@@ -104,4 +120,4 @@ celsiusLink.addEventListener("click", convertToCelsius);
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-displayForecast();
+

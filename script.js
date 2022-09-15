9edce659b90c50878db16e4655629071fbadf8ec
DMedlock <danielleymedlock@gmail.com>
2022-09-15 13:00:07 -0500

    changes api link from http to https

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "1dbf926d3b4417bf379db7043bec1047";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
